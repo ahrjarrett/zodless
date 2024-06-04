84986c16dd0375abec74047ee64ffc7b9fc35eea
Andrew Jarrett <ahrjarrett@gmail.com>
2024-06-04 02:19:56 -0500

    chore: cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export { z } from "./schema.js"
+export { Parser } from "./parser.js"
+export { ZODLESS_VERSION } from "./version.js"
---
+++ src/version.ts
@@ -0,0 +1,2 @@
+export const ZODLESS_VERSION = "0.0.1"
+export type ZODLESS_VERSION = typeof ZODLESS_VERSION
