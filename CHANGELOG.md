bb9c07327434c47f402a77ee467e3201bd109241
Andrew Jarrett <ahrjarrett@gmail.com>
2024-06-04 02:32:11 -0500

    bump: v0.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# zodless
+
+## 0.0.2
+
+### Patch Changes
+
+- b509807: expose utils: `array`, `fn`, `map` and `object` modules
--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const ZODLESS_VERSION = "0.0.1"
+export const ZODLESS_VERSION = "0.0.2" as const
 export type ZODLESS_VERSION = typeof ZODLESS_VERSION
